c0dfd65c0e1a1d530598e26b99c3fc2169640107
Aleksandr <sanyaveremeev9@gmail.com>
2025-02-09 16:00:01 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,52 +1,118 @@
 import telebot
 import config
-# import random
-# from telebot import types
+import random
+from telebot import types
+
+# добавить клавиатуру, (рандомное число, как дела) markup
 
 bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
-	bot.send_message(message.chat.id, 'Hello my friend! \nНапиши число, и я скажу простое ли оно!')
+
+    # keyboard
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("🎲 Рандомное число")
+    item2 = types.KeyboardButton("😊 Как дела?")
+    markup.add(item1, item2)
+
+    bot.send_message(message.chat.id, "Hello my friend! \nЯ бот, который знает какие числа простые, а какие нет. \nНапиши любое число, и я скажу тебе: простое оно или нет!".format(message.from_user, bot.get_me()), parse_mode='html', reply_markup=markup)
     # (message.chat.id, 'Привет!')
 
 @bot.message_handler(content_types=['text'])
 def lalala(message):
     # найти в message.text числа, если есть - то перевод на поиск выбранного вопроса.
     # если числа нет, то другое - (привет, пока, как дела, сколько времени)
-    message2 = ''
-    m1 = ''
-    r = True
+    message_end = ''
+    number = ''
+    cikl = True
     if message.text.isdigit():
         # если сообщение - число
         try:
             # преобразование
-            m1 = int(message.text)
+            number = int(message.text)
         except:
-            r = False
-            m1 = 1
-        
+            cikl = False
+            number = 1
         # если все ок, то отправляем сообщение
-        if r:
+        if cikl:
             # отправка сообщения
-            a = isprime(m1)
-            if a:
-                 message2 = 'Это число простое!'
-            elif a == False:
-                 message2 = 'Это число составное!'
-    else:
-        message2 = message.text
-        
-            
-    # end message
-    bot.send_message(message.chat.id, message2)
+            mess = isprime(number)
+            if mess:
+                message_end = 'число ' + str(number) + ' простое'
+                # число X простое
+                # число X составное
+                # это число -простое -составное
+                # это число составное, так как 2 * 3 = 6.
+                #  
+            elif mess == False:
+                message_end = 'число ' + str(number) + ' составное'
+                # из функции isprime найти 
+    elif message.chat.type == 'private':
+        if message.text == '🎲 Рандомное число':
+            bot.send_message(message.chat.id, str(random.randint(0, 100)))
+        elif message.text == '😊 Как дела?':
 
-# простое число или нет
+            markup = types.InlineKeyboardMarkup(row_width=2)
+            item1 = types.InlineKeyboardButton("Хорошо", callback_data='good')
+            item2 = types.InlineKeyboardButton("Не очень", callback_data='bad')
+
+            markup.add(item1, item2)
+
+            bot.send_message(message.chat.id, 'Отлично, сам как?', reply_markup=markup)
+        else:
+            pass
+            # bot.send_message(message.chat.id, 'Я не знаю что ответить')
+        # если число не указано
+        # message_end = "Вы не выбрали никакого числа!"
+
+    # отправка ответа
+    if message_end != '':
+        # end message
+        bot.send_message(message.chat.id, message_end)
+
+# функция определения простого числа, методом while.
 def isprime(n):
     d = 2
     while n % d != 0:
         d += 1
     return d == n
 
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    try:
+        if call.message:
+            if call.data == 'good':
+                bot.send_message(call.message.chat.id, 'Вот и отличненько 😊')
+            elif call.data == 'bad':
+                bot.send_message(call.message.chat.id, 'Бывает 😢')
+
+            # remove inline buttons
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="😊 Как дела?",
+                reply_markup=None)
+ 
+            # show alert
+            # bot.answer_callback_query(callback_query_id=call.id, show_alert=False, text="ТЕСТ")
+    
+    except Exception as e:
+        print(repr(e))
+
 # RUN
 bot.polling(none_stop=True)
+
+# Работает:
+# приветствие +(ток при /start)
+
+# TODO:
+# зависание, подвисание.
+# больше текста добавить.
+# функцию добавить (1, 2)
+
+# добавление функций...
+# изменить функцию isprime так, чтобы можно было получить числа, составляющие.
+# 1. показывает числа из которых состоит, в случае если оно составное
+# 2. показывает док. что оно простое
+# -+Добавить функцию рандомного числа (например чтобы потом определить уже на нём простое оно или нет)
+# 3. Добавить функцию генерации пароля, по заданным вариантам (только числа, только буквы, символы...)
+# 3.1. Проверка пароля - хороший пароль или плохой.
+# Экспериментально: проверка файла на наличие вирусов.
